3c0258a9d4d7685ee65c455fd3927562afdb6e4f
karol-202 <karoljurski1@gmail.com>
2020-06-03 00:17:41 +0200

    Remove non-swing modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,6 @@ allprojects {
 
     repositories {
         jcenter()
+        mavenLocal()
     }
 }
---
+++ uranium-swing/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	kotlin("jvm") version "1.3.72"
+	`maven-publish`
+}
+
+dependencies {
+	api("pl.karol202.uranium:uranium-core:0.1")
+
+	implementation(kotlin("stdlib-jdk8"))
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.3.7")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.register<Jar>("sourcesJar") {
+	archiveClassifier.set("sources")
+	from(sourceSets.main.get().allSource)
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("swing") {
+			from(components["java"])
+			artifact(tasks["sourcesJar"])
+		}
+	}
+}
