23871fb15790fcaf8f3191bfb8d43672030e71e0
karol-202 <karoljurski1@gmail.com>
2020-06-04 17:18:37 +0200

    Publish in bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    kotlin("jvm") version "1.3.72" apply false
+    id("com.jfrog.bintray") version "1.8.5" apply false
+}
+
 allprojects {
     group = "pl.karol202.uranium"
     version = "0.2"
--- uranium-swing/build.gradle.kts
+++ uranium-swing/build.gradle.kts
@@ -1,7 +1,9 @@
+import com.jfrog.bintray.gradle.BintrayExtension.PackageConfig
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("jvm") version "1.3.72"
+	kotlin("jvm")
+	id("com.jfrog.bintray")
 	`maven-publish`
 }
 
@@ -22,11 +24,49 @@ tasks.register<Jar>("sourcesJar") {
 	from(sourceSets.main.get().allSource)
 }
 
+bintray {
+	user = project.findProperty("bintray.user") as String?
+	key = project.findProperty("bintray.key") as String?
+	setPublications("uraniumSwing")
+	publish = true
+
+	pkg(delegateClosureOf<PackageConfig> {
+		repo = "uranium"
+		name = "uranium-swing"
+		description = "Uranium for Swing"
+		vcsUrl = "https://github.com/karol-202/uranium-swing"
+		githubRepo = "karol-202/uranium-swing"
+		setLicenses("MIT")
+	})
+}
+
 publishing {
 	publications {
-		create<MavenPublication>("uranium-swing") {
+		create<MavenPublication>("uraniumSwing") {
 			from(components["java"])
 			artifact(tasks["sourcesJar"])
+
+			pom {
+				name.set(bintray.pkg.name)
+				description.set(bintray.pkg.desc)
+				url.set(bintray.pkg.vcsUrl)
+				licenses {
+					license {
+						name.set("MIT")
+						url.set("https://opensource.org/licenses/MIT")
+					}
+				}
+				developers {
+					developer {
+						id.set("karol202")
+						name.set("Karol Jurski")
+						email.set("karoljurski1@gmail.com")
+					}
+				}
+				scm {
+					url.set(bintray.pkg.vcsUrl)
+				}
+			}
 		}
 	}
 }
